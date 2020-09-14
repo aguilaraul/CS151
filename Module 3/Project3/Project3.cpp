84c226e3515742ce7ee458c8a1288c5c8cc47c89
Raul Aguilar <raula@live.com>
2020-09-13 18:35:57 -0700

    Module 3:Project 3
    Module 3 projects completed.

--- Module 3/Project3/Project3.cpp
+++ Module 3/Project3/Project3.cpp
@@ -14,7 +14,11 @@
 * 2. Input 30 respones of the number of pie slices eaten in a year
 * 3. Sort array into ascending order
 * 4. Find mode by counting the number of slices with the most responses
-*	
+*	Count the number of occurances of the current number.
+*	If a new number is scanned, and if current count is greater than the
+*	total, then a new mode is found.
+*	Set total to current count and set mode to current number
+*	Set the current number to the new number scanned and reset count
 */
 #include <iostream>
 
@@ -25,56 +29,63 @@ void printRespones(int[], int);
 void bubbleSort(int[], int);
 void swap(int&, int&);
 
-const int SIZE = 5;
+const int SIZE = 30;
 
 int main()
 {
 	// Dynamically allocate array of respones
-	int* respones = new int[SIZE];
+	int* responses = new int[SIZE];
 
 	// Input number pie slices eaten by each person per year
 	cout << "Enter number of pie slices each person eats in a year:" << endl;
 	for (int i = 0; i < SIZE; i++)
 	{
-		cin >> *(respones + i);
+		cin >> *(responses + i);
 	}
 
 	// Sort array into ascending order
-	bubbleSort(respones, SIZE);
+	bubbleSort(responses, SIZE);
+
+	// Print sorted responses
+	printRespones(responses, SIZE);
 
 	// Find mode of respones
 	cout << "The number of slices with the most responses:" << endl;
-	cout << mode(respones, SIZE) << endl;
+	cout << mode(responses, SIZE) << endl;
 
-	// Print sorted responses
-	printRespones(respones, SIZE);
-
-	delete[] respones;
-	respones = nullptr;
+	delete[] responses;
+	responses = nullptr;
 	return 0;
 }
 
 int mode(int array[], int size)
 {
-	int mode;
-	int total = 0;
+	int mode = array[0];
+	int total = 1;
 	int current = array[0];
 	int count = 1;
 
 	for (int i = 1; i < size; i++)
 	{
+		// Count the number of occurances of the current number
+		// If current number is equal to number in array, then increase tally
 		if (current == array[i])
 		{
 			count++;
-		} else
+		}
+		// Current number is differnt than number in array
+		// Tally streak broken
+		else
 		{
+			// If the current count is greater than the total, then new mode is
+			// found. Set total to current count and set mode to current number
 			if (count > total)
 			{
 				total = count;
-				mode = array[i - 1];
+				mode = current;
 			}
-			current = array[i];
-			count = 1;
+			current = array[i]; // set current number to new number
+			count = 1; // reset count
 		}
 	}
 	return mode;
