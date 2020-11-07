bfea64cb2e545e3fc2e74b92199f2d47db2f8baa
Raul Aguilar <raula@live.com>
2020-11-06 21:00:21 -0800

    Module 8:Lab
    Lab 8 project 1 under works. Got the gist of it down, now I need to make it loop

---
+++ Module 8/Lab/Project 1/lab8_project1.cpp
@@ -0,0 +1,54 @@
+/**
+ * lab8_project1.cpp
+ *
+ * Author:  Raul Aguilar
+ * Date:    November 6, 2020
+ *
+ * CS 151 3228 Module 8 Lab Project 1
+ * Encrypt a string using a lambda function.
+ *
+ */
+#include <iostream>
+#include <algorithm>
+#include <strings.h>
+using namespace std;
+
+void encrypt(char& c) {
+    if(isalnum(c)) {
+        if(c == 'z') {
+            c = 'a';
+        } else if(c == 'Z') {
+            c = 'A';
+        } else if(c == '9') {
+            c = '0';
+        } else {
+            c++;
+        }
+    }
+}
+
+int main() {
+    string input;
+    string test;
+
+    cout << "Enter a string to encrypt:" << endl;
+    getline(cin, input);
+
+    test = input;
+    for_each(test.begin(), test.end(),
+             [](char &c) { c = tolower(c); } );
+
+    if(test != "quit") {
+        cout << input << endl;
+    } else {
+        cout << "Program exiting..." << endl;
+        exit(0);
+    }
+
+    for_each(input.begin(), input.end(),
+            [](char &c) { encrypt(c); } );
+
+    cout << input << endl;
+
+    return 0;
+}
